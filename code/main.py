2d3b6ea684834892845e69450e69d09759cee739
22024-Gus <22024@nayland.school.nz>
2026-05-11 10:50:36 +1200

    v1
    setup

--- code/main.py
+++ code/main.py
@@ -1,29 +1,35 @@
-from settings import *
+from settings import * # importing variables, constants, etc. from settings.py
+from pytmx.util_pygame import load_pygame
+from os.path import join
 
 class Game:
     def __init__(self):
         pygame.init()
-        self.display_surface = pygame.display.set_mode((WIN_WID, WIN_HGT))
-        pygame.display.set_caption("Unesco pokemon game")
-        self.clock = pygame.time.Clock()
-        self.running = True
+        self.display_surf = pygame.display.set_mode((WIN_WID, WIN_HGT)) # setting resolution of window
+        pygame.display.set_caption('epic game') # sets caption of game window
+
+        self.import_assets()
+        self.setup(self.tmx_maps['world'], 'house')
+
+    def import_assets(self):
+        self.tmx_maps = {'world': load_pygame(join('Unesco-game-2026', 'data','maps','test.tmx'))}
+        print(self.tmx_maps)
+
+    def setup(self, tmx_map, player_start_pos):
+        for x,y, surf in tmx_map.get_layer_by_name('Terrain').tiles():
+            print(x,y,surf)
 
     def run(self):
-        while self.running:
-            dt = self.clock.tick() / 1000 # deltatime
+        while True:
+            #event loop
+            for event in pygame.event.get(): # check for pygame events
+                if event.type == pygame.QUIT: # if player closes main window:
+                    pygame.quit() # quit the game
+                    exit()
 
-            #event
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    self.running = False
+            #game logic
+            pygame.display.update() # update display
 
-            #update
-
-            #draw
-            pygame.display.update()
-
-        pygame.quit()
-
-if __name__ == '__main__':
+if __name__ == '__main__': # initialise game (failsafe; checks if this file is called main)
     game = Game()
-    game.run()
+    game.run()  
--- code/settings.py
+++ code/settings.py
@@ -1,6 +1,29 @@
 import pygame
-from os.path import join
-from os import walk
+from pygame.math import Vector2 as vector
+from sys import exit
 
-WIN_WID, WIN_HGT = 640,480
-TILE_SIZE = 32
+WIN_WID, WIN_HGT = 960,720
+TILE_SIZE = 64
+ANIM_SPEED = 6
+BATTLE_OUTLINE_WID = 4
+
+COLOURS = { 
+    'white': '#f4fefa',
+    'pure white': '#ffffff',
+    'dark': '#2b292c',
+    'light': '#c8c8c8',
+    'grey': '#3a373b',
+                
+}
+
+WORLD_LAYERS = {
+}
+
+BATTLE_POSITIONS = {
+}
+
+BATTLE_LAYERS = {
+}
+
+BATTLE_CHOICES = {
+}
